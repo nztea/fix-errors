2638abff9f1fa2ffb840e02c41ea9ae87bbadfe8
Peter Horton <phorton@softstackfactory.org>
2018-02-13 04:52:23 +0000

    function name and definition

--- lesson9.js
+++ lesson9.js
@@ -8,10 +8,10 @@ var people = [
     { name: "John", age: 55 },
     { name: "Roger", age: 44 },
     { name: "Betty", age: 37 }
-    ]
+];
 
 var nameFilter = function(name) {
-    let name = "Eric"
+    let filteredName = name;
     for (let i = 0; i < people.length; i++) {
         let name = people[i].name
         if (name != filteredName) {
