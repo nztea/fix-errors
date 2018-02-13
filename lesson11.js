1ab37b893866b19f40ccbdfc749cba0aa8de3eae
Peter Horton <phorton@softstackfactory.org>
2018-02-13 05:20:25 +0000

    return statement

--- lesson11.js
+++ lesson11.js
@@ -5,16 +5,17 @@ var people = [
     { name: "John", age: 55 },
     { name: "Roger", age: 44 },
     { name: "Betty", age: 37 }
-    ]
+]
 
-var nameFilter = function(filteredName) {
+var nameFilter = function(filteredNames) {
     var results = []
     for (var i = 0; i < people.length; i++) {
         let name = people[i].name
-        if (name != filteredName) {
+        if (name != filteredNames) {
             results.push(name)
         }
     }
+    return results;
 }
 
 var filteredNames = nameFilter("Betty")
